a35a8f3022f0f9d1aabf9135fb0a74b30e1bd19a
Sergio <sergio_castilla_13@hotmail.com>
2021-03-09 16:47:02 +0100

    Ejercicio con Gradle

---
+++ ud23gradleDemo/src/main/java/com/example/demo/Ud23gradleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ud23gradleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ud23gradleDemoApplication.class, args);
+	}
+
+}
